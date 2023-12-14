9c49dd0664788aca9b4d1ee328cca6b37cf5bb0a
hexaalif <shadullahsad9@gmail.com>
2023-12-14 15:20:27 +0600

    login form selected

--- pages/login.js
+++ pages/login.js
@@ -14,44 +14,96 @@ const login = () => {
           </h1>
         </div>
       </div>
-      <div className="">
-        <div class="background">
-          <div class="shape"></div>
-          <div class="shape"></div>
-        </div>
-        <form id="form">
-          <h3>Login Here</h3>
 
-          <label id="label" for="username">
-            Username
+      {/* login form htmml */}
+      <div class="login-wrap mx-auto my-10">
+        <div class="login-html">
+          <input id="tab-1" type="radio" name="tab" class="sign-in" checked />
+          <label for="tab-1" class="tab">
+            Sign In
           </label>
-          <input
-            className="input"
-            type="text"
-            placeholder="Email or Phone"
-            id="username"
-          />
-
-          <label id="label" for="password">
-            Password
+          <input id="tab-2" type="radio" name="tab" class="sign-up" />
+          <label for="tab-2" class="tab">
+            Sign Up
           </label>
-          <input
-            className="input"
-            type="password"
-            placeholder="Password"
-            id="password"
-          />
-
-          <button id="btn">Log In</button>
-          <div class="social">
-            <div class="go">
-              <i class="fab fa-google"></i> Google
+          <div class="login-form">
+            <div class="sign-in-htm">
+              <div class="group">
+                <label for="user" class="label">
+                  Username
+                </label>
+                <input id="user" type="text" class="input" />
+              </div>
+              <div class="group">
+                <label for="pass" class="label">
+                  Password
+                </label>
+                <input
+                  id="pass"
+                  type="password"
+                  class="input"
+                  data-type="password"
+                />
+              </div>
+              <div class="group">
+                <input id="check" type="checkbox" class="check" checked />
+                <label for="check">
+                  <span class="icon"></span> Keep me Signed in
+                </label>
+              </div>
+              <div class="group">
+                <input type="submit" class="button" value="Sign In" />
+              </div>
+              <div class="hr"></div>
+              <div class="foot-lnk">
+                <a href="#forgot">Forgot Password?</a>
+              </div>
             </div>
-            <div class="fb">
-              <i class="fab fa-facebook"></i> Facebook
+            <div class="sign-up-htm">
+              <div class="group">
+                <label for="user" class="label">
+                  Username
+                </label>
+                <input id="user" type="text" class="input" />
+              </div>
+              <div class="group">
+                <label for="pass" class="label">
+                  Password
+                </label>
+                <input
+                  id="pass"
+                  type="password"
+                  class="input"
+                  data-type="password"
+                />
+              </div>
+              <div class="group">
+                <label for="pass" class="label">
+                  Repeat Password
+                </label>
+                <input
+                  id="pass"
+                  type="password"
+                  class="input"
+                  data-type="password"
+                />
+              </div>
+              <div class="group">
+                <label for="pass" class="label">
+                  Email Address
+                </label>
+                <input id="pass" type="text" class="input" />
+              </div>
+              <div class="group">
+                <input type="submit" class="button" value="Sign Up" />
+              </div>
+              <div class="hr"></div>
+              <div class="foot-lnk">
+                <label for="tab-1">Already Member?</label>
+              </div>
             </div>
           </div>
-        </form>
+        </div>
       </div>
     </div>
   );
